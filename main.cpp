c2c2e8f790d1874d5c1ba9fa4eab3426cc08f846
Charles Bai <charlesbai321@gmail.com>
2018-10-21 15:42:55 -0700

    taking command line arguments

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	if(argc != 2){
+		cout << "usage: " << argv[0] << " <filename>" << endl;
+		return 0;
+	}
+	
+	return 0;
+}
